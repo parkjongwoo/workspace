527dfa975db21d561bd776b82e5c57e6f5793781
parkjongwoo <shoong1999@gmail.com>
2018-08-28 04:16:34 +0900

    ignore추가, 알고리즘문제 진행

--- 0101.DI/src/main/java/spring/ex02/HakingList.java
+++ 0101.DI/src/main/java/spring/ex02/HakingList.java
@@ -1,59 +1,124 @@
 package spring.ex02;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class HakingList {
-	
+
 	public HakingList() {
-		
+		showMemory();
+//		long start = new Date().getTime();
+//		System.out.println(start);		
 		Branch request = getRequestBranch();
-		List<Branch> list = new ArrayList<Branch>();
+		int max = request.left;
+		Map<String,Branch> map = new HashMap<String,Branch>();
+		Map<Branch,Integer> root = new HashMap<Branch,Integer>();
+		Map<Branch,Integer> reaf = new HashMap<Branch,Integer>();
+		Map<Branch,Integer> body = new HashMap<Branch,Integer>();
 		
-		System.out.println("컴퓨터대수:"+request.left+" 신뢰갯수:"+request.right);
-		StringBuffer sb = new StringBuffer();
-		for(int i=0;i<request.right;i++) {
-			Branch b = getBranch();
-			list.add(b);
-			sb.append(b).append("   ");
-			if(i%5==0) {
-				sb.append('\n');
+		System.out.println(request.toString());
+		for (int i = 0; i < request.right; i++) {
+			Branch b = getBranch(max);
+			String key = b.toString();
+			if (map.get(key) != null) {
+				i--;
+				continue;
+			}
+			map.put(key, b);
+			root.put(b, b.right);
+			reaf.put(b, b.left);
+		}
+		System.out.println("root.size()+ reaf.size():"+(root.size()+ reaf.size()));
+		boolean b = false;
+		for( Entry<String,Branch> e : map.entrySet()) {	
+//			System.out.println("e.getValue().left:"+e.getValue().left);
+//			root.get()
+			
+			if(b = root.get(e.getValue()).intValue()==e.getValue().left)
+				root.remove(e.getValue());
+			if(b = reaf.get(e.getValue()).intValue()==e.getValue().right)
+				reaf.remove(e.getValue());
+						
+			if(b) {
+				body.put(e.getValue(),e.getValue().left);
 			}
 		}
 		
-		System.out.println(sb);
+		
+		
+		int total = root.size()+ reaf.size()+body.size();
+		System.out.println("root.size():"+root.size()+" reaf.size():"+reaf.size()+" body.size():"+body.size());
+		System.out.println("total:"+map.size() + " | "+total);
+//		long end = new Date().getTime();
+//		System.out.println(end - start);
+		showMemory();
 	}
-	
+	static Runtime r = Runtime.getRuntime();
+	public static void showMemory() {
+
+		DecimalFormat format = new DecimalFormat("###,###,###.##");
+
+		// JVM이 현재 시스템에 요구 가능한 최대 메모리량, 이 값을 넘으면 OutOfMemory 오류가 발생 합니다.
+
+		long max = r.maxMemory();
+
+		// JVM이 현재 시스템에 얻어 쓴 메모리의 총량
+
+		long total = r.totalMemory();
+
+		// JVM이 현재 시스템에 청구하여 사용중인 최대 메모리(total)중에서 사용 가능한 메모리
+
+		long free = r.freeMemory();
+
+		System.out.println(
+				"Max:" + format.format(max) + ", Total:" + format.format(total) + ", Free:" + format.format(free));
+
+	}
+
 	private Branch getRequestBranch() {
-		return new Branch(randomIntN(), randomIntM());
+		return new Branch(randomIntN(10000), randomIntN(100000));
 	}
-	private Branch getBranch() {
-		return new Branch(randomIntN(), randomIntN());
+
+	private Branch getBranch(int max) {
+		int left = randomIntN(max);
+		int right = randomIntN(max, left);
+		return new Branch(left, right);
 	}
-	private int randomIntN() {
-		return (int)(Math.round(Math.random() * 10000) + 1);
-	}
-	private int randomIntM() {
-		return (int)(Math.round(Math.random() * 100000) + 1);
-	}	
 	
+	private int randomIntN(int max) {
+		return (int) (Math.round(Math.random() * max) + 1);
+	}	
+
+	private int randomIntN(int max, int left) {
+		int right = randomIntN(max);
+		if (left == right)
+			randomIntN(max, left);
+		return right;
+	}	
+
 	public static void main(String[] args) {
 		new HakingList();
 	}
-	
-	class Branch{
+
+	class Branch {
 		public int left;
 		public int right;
-		
-		public Branch() {}
+
+		public Branch() {
+		}
+
 		public Branch(int left, int right) {
 			this.left = left;
 			this.right = right;
 		}
-		
+
 		@Override
 		public String toString() {
-			return left+":"+right;
+			return left + " " + right;
 		}
 	}
 }
