f397dfa4e7d8f21561dd880138167201a3e65a97
parkjongwoo <shoong1999@gmail.com>
2018-10-01 18:10:23 +0900

    algo
    samsung

---
+++ QProject/src/Pinball.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pinball {
+	
+	public void proc() {
+		Scanner s = new Scanner(System.in);
+		
+		int cases = s.nextInt();		
+				
+				
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Pinball();
+	}
+
+}
