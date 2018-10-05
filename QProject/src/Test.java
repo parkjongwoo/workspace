84afc02eeb80dac2b1ce487e19c017ef667c1532
parkjongwoo <shoong1999@gmail.com>
2018-10-05 18:14:25 +0900

    drfgdf
    fdgdfg

---
+++ QProject/src/Test.java
@@ -0,0 +1,19 @@
+class Test {
+
+     public static void main (String[] args){
+          
+          int result = 1 + 2; 
+
+          result = result - 1; 
+
+          result = result * 2; 
+
+          result = result / 2; 
+
+          result = result + 8; 
+
+          result = result % 7; 
+
+          System.out.println(result);
+     }
+}
